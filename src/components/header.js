fd3909ba418fd7f3b4ad55a0f51d0841ba8b9ca7
OMAR <ojramirezv@gmail.com>
2022-10-04 08:07:55 -0500

    addressing codechallenge2 waste not

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+export default function Header(props){
+    const type = (props.type) ? `is-${props.type}` : 'is-success';
+    return(
+        <section className={`hero ${type}`}>
+        <div className="hero-body">
+          <p className="title">
+            {props.title}
+          </p>
+          <p className="subtitle">
+            {props.subtitle}
+          </p>
+        </div>
+      </section>
+    )
+}
